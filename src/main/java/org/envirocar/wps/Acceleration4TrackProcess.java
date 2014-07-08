4033e7ac27dff328aba2372dcab4047308590626
staschc <staschc@uni-muenster.de>
2014-07-08 15:02:04 +0200

    Bug fixing
    Bug fixing for iterating over featuers and computing delta time

--- src/main/java/org/envirocar/wps/Acceleration4TrackProcess.java
+++ src/main/java/org/envirocar/wps/Acceleration4TrackProcess.java
@@ -24,10 +24,12 @@
 package org.envirocar.wps;
 
 import java.net.URL;
+import java.text.ParseException;
+import java.util.Date;
 
-import org.envirocar.wps.util.BasicStats;
 import org.envirocar.wps.util.EnviroCarFeatureParser;
 import org.envirocar.wps.util.EnviroCarWpsConstants;
+import org.envirocar.wps.util.EnviroCarWpsConstants.FeatureProperties;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.n52.wps.algorithm.annotation.Algorithm;
@@ -38,6 +40,8 @@ import org.n52.wps.server.AbstractAnnotatedAlgorithm;
 import org.opengis.feature.simple.SimpleFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 
 
 /**
@@ -95,14 +99,12 @@ import org.slf4j.LoggerFactory;
 			SimpleFeatureCollection features = parser.createFeaturesFromJSON(url);
 			SimpleFeatureIterator featIter = features.features();
 			try {
+				SimpleFeature featFirst = featIter.next();
 				while (featIter.hasNext()){
-					SimpleFeature feat1 = featIter.next();
-					if (featIter.hasNext()){
-						SimpleFeature feat2 = featIter.next();
-						sum += BasicStats.calculateAcceleration(feat1, feat2);
-						counter++;
-					}
-					
+					SimpleFeature featSecond = featIter.next();
+					sum += calculateAcceleration(featFirst, featSecond);
+					featFirst=featSecond;
+					counter++;
 				}
 			} catch (Exception e){
 				LOGGER.debug("Error while extracting speed attribute from features: "+e.getLocalizedMessage());
@@ -113,6 +115,51 @@ import org.slf4j.LoggerFactory;
 			}
 			return sum/counter;
 		}	
+		
+		/**
+		 * calculates acceleration (delta_speed/delta_time in m/s^2) between two single
+		 * track measurements, represented as Geotools simple features
+		 * 
+		 * 
+		 * @param feat1
+		 *            first track measurement (must be temporally before second)
+		 * @param feat2
+		 *            second track measurement
+		 * @return acceleration between first and second measurement
+		 * @throws ParseException 
+		 */
+		public double calculateAcceleration(SimpleFeature feat1,
+				SimpleFeature feat2) throws ParseException {
+			double acceleration = 0;
+			
+			//compute delta t in seconds
+			ISO8601DateFormat df = new ISO8601DateFormat();
+			Date timeStart = df.parse((String)feat1.getAttribute(FeatureProperties.TIME));
+			Date timeEnd = df.parse((String)feat2.getAttribute(FeatureProperties.TIME));
+			long delta_t = (timeEnd.getTime()-timeStart.getTime()) / 1000;
+			
+			//compute delta speed in m/s
+			double delta_speed = 0;
+			Object spo1 = feat1.getAttribute(FeatureProperties.SPEED);
+			Object spo2 = feat2.getAttribute(FeatureProperties.SPEED);
+			
+			//compute delta speed in km/h
+			if (spo1!=null && spo2!=null){
+				 double speed1 = Double.parseDouble((String)spo1);
+				 double speed2 = Double.parseDouble((String)spo2);
+				if (speed2>speed1){
+					delta_speed= speed2-speed1;
+				}
+			}
+			
+			//convert to m/s
+			delta_speed = delta_speed * 0.278;
+			
+			//compute acceleration in m/s^2
+			acceleration = delta_speed/delta_t;
+			
+			return acceleration;
+		}
 	    
 	    
 
