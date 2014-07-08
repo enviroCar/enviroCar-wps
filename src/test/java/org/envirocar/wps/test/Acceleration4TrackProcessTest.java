b0305ba7541490338ca83e6b6504a9d755676587
staschc <staschc@uni-muenster.de>
2014-07-08 15:02:57 +0200

    Assertion updated to corrected acceleration number

--- src/test/java/org/envirocar/wps/test/Acceleration4TrackProcessTest.java
+++ src/test/java/org/envirocar/wps/test/Acceleration4TrackProcessTest.java
@@ -43,7 +43,7 @@ public class Acceleration4TrackProcessTest {
 		Acceleration4TrackProcess process = new Acceleration4TrackProcess();
 		try {
 			double acceleration = process.getAcceleration4Track("539e0127e4b01607fa47bfd5");
-			assertEquals(acceleration,4.677647008547006E-8,0.00002);
+			assertEquals(0.04738652564102559,acceleration,0.00002);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
